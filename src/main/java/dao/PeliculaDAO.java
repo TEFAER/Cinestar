b977e393e18a1f5ebaf7597096c0aaea5178658f
USER <USER@DESKTOP-TJSJ0HQ>
2023-02-26 16:58:22 -0500

    Actualizacion de packetes src

--- src/main/java/dao/PeliculaDAO.java
+++ src/main/java/dao/PeliculaDAO.java
@@ -1,5 +1,25 @@
 package dao;
 
-public class PeliculaDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import bean.Pelicula;
+
+public class PeliculaDAO {
+	db.Db db= new db.Db("CineStar");
+
+	public Object getPeliculas(int id, boolean bLista) {
+	db.Sentencia( String.format("call sp_getPeliculas(%s)", id));
+	String[ ][ ] peliculas = db.getRegistros();
+	if(peliculas ==null)return null;
+	if (!bLista) return peliculas;
+	
+	List<Pelicula> lstPeliculas = new ArrayList<>();
+	for(String[ ] pelicula : peliculas)
+		lstPeliculas.add(new Pelicula(pelicula));
+	
+	return lstPeliculas;
+	
+	
+	} 
 }
--- src/main/java/svl/svlPelicula.java
+++ src/main/java/svl/svlPelicula.java
@@ -1,40 +1,47 @@
 package svl;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
-/**
- * Servlet implementation class svlPelicula
- */
 public class svlPelicula extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+ 
     public svlPelicula() {
         super();
-        // TODO Auto-generated constructor stub
+    
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	response.setContentType("text-html;charset=UTF-8");
+    	response.setCharacterEncoding("UTF-8");
+    	HttpSession session = request.getSession();
+    	
+    	String id =request.getParameter("id");
+    	if(id !=null && (id.equals("cartelera") || id.equals("estrenos"))) {
+     
+    		Object data =new dao.PeliculaDAO().getPeliculas(id.equals("cartelera")? 1 : 2 , true);
+    		session.setAttribute("id", data == null? null :  "peliculas");
+    		session.setAttribute("data", data);
+    }
+    	
+    	response.sendRedirect("index.jsp");
+    	
+    	
+    	
+    }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		processRequest(request,response);
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		processRequest(request,response);
 	}
 
 }
+
